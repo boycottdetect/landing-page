d644f611c6177a1476f1931a4b2a82826ee9ca7b
Juan Pablo Abara <jpabara1@gmail.com>
2025-01-09 07:09:48 -0300

    Created landing page with brands list search bar

--- js/loadDB.js
+++ js/loadDB.js
@@ -6,10 +6,10 @@ const loadDB = async () => {
     brandsDB = jsonData
 }
 
-const renderTable = (brands) => {
+const renderTable = (brandsDB) => {
     const brandsTable = document.getElementById('brands-table')
-    for (const brand of Object.values(brands)) {
-        console.log(brand)
+    brandsTable.innerHTML = ''
+    for (const brand of brandsDB) {
         const brandDiv = document.createElement('div')
         brandDiv.classList.add('brand-div')
         const brandName = document.createElement('h3')
@@ -18,25 +18,48 @@ const renderTable = (brands) => {
         const explanation = document.createElement('p')
         explanation.textContent = brand.explanation
         brandDiv.appendChild(explanation)
+        if (brand.subbrands.length > 0) {
+            const subbrandsTitle = document.createElement('h4')
+            subbrandsTitle.textContent = `Otras marcas que pertenecen a ${brand.companyName}`
+            brandDiv.appendChild(subbrandsTitle)
+            const subbrands = document.createElement('ul')
+            for (const subbrand of brand.subbrands) {
+                if (subbrand === '') continue
+                const subbrandLi = document.createElement('li')
+                subbrandLi.textContent = subbrand
+                subbrands.appendChild(subbrandLi)
+            }
+            brandDiv.appendChild(subbrands)
+        }
         brandsTable.appendChild(brandDiv)
-        if (brand.subbrands.length === 0) {
-            continue
-        }
-        const subbrandsTitle = document.createElement('h4')
-        subbrandsTitle.textContent = `Otras marcas que pertenecen a ${brand.companyName}`
-        brandDiv.appendChild(subbrandsTitle)
-        const subbrands = document.createElement('ul')
-        for (const subbrand of brand.subbrands) {
-            const subbrandLi = document.createElement('li')
-            subbrandLi.textContent = subbrand
-            subbrands.appendChild(subbrandLi)
-        }
-        brandDiv.appendChild(subbrands)
     }
+}
+
+const searchBrands = () => {
+    const searchInput = document.getElementById('search-input')
+    const searchValue = searchInput.value
+    const filteredBrands = []
+    for (const brand of Object.values(brandsDB)) {
+        if (brand.companyName.toLowerCase().includes(searchValue.toLowerCase())) {
+            filteredBrands.push(brand)
+        }
+        for (const subbrand of brand.subbrands) {
+            if (subbrand.toLowerCase().includes(searchValue.toLowerCase())) {
+                filteredBrands.push(brand)
+                break
+            }
+        }
+    }
+    renderTable(filteredBrands)
+}
+
+const openBrands = () => {
+    window.open('brands_list.html', '_self')
 }
 
 window.onload = async () => {
     await loadDB()
-    console.log("BRANDS DB", brandsDB)
-    renderTable(brandsDB)
+    renderTable(Object.values(brandsDB))
+    const searchInput = document.getElementById('search-input')
+    searchInput.addEventListener('input', searchBrands)
 }
